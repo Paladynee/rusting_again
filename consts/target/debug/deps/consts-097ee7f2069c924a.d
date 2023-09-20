022b303eb74189014db161c23ab0efcc3ead1d53
Voxell Paladyne <paladyneshadow@gmail.com>
2023-09-21 02:42:47 +0300

    commit first lmao

---
+++ consts/target/debug/deps/consts-097ee7f2069c924a.d
@@ -0,0 +1,5 @@
+C:\Users\Voxell\Desktop\deving\workspace\rusting_again\consts\target\debug\deps\consts-097ee7f2069c924a.rmeta: src\main.rs
+
+C:\Users\Voxell\Desktop\deving\workspace\rusting_again\consts\target\debug\deps\consts-097ee7f2069c924a.d: src\main.rs
+
+src\main.rs:
---
+++ consts/target/debug/deps/consts-f90977175ec3f8f9.d
@@ -0,0 +1,5 @@
+C:\Users\Voxell\Desktop\deving\workspace\rusting_again\consts\target\debug\deps\consts-f90977175ec3f8f9.rmeta: src\main.rs
+
+C:\Users\Voxell\Desktop\deving\workspace\rusting_again\consts\target\debug\deps\consts-f90977175ec3f8f9.d: src\main.rs
+
+src\main.rs:
